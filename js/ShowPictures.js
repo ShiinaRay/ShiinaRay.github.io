6f462b1b2099da8ae5d7a21c0aa82f7fe5f30aaf
Xzfer <Xzfer@github.com>
2021-04-11 11:47:30 +0800

    Initial commit

---
+++ js/ShowPictures.js
@@ -0,0 +1,32 @@
+var apple = document.getElementById("apple");
+var ray = document.getElementById("ray");
+var silver = document.getElementById("silver");
+
+
+apple.addEventListener("click",picLink);
+ray.addEventListener("click",picLink);
+silver.addEventListener("click",picLink);
+
+function picLink(){
+    var allImages = document.querySelectorAll(".ShowPictures img");    
+    //var allImages = document.querySelectorAll("img");  
+    // 之前把所有的  img标签都选中了    
+// f12  网页端  检查   出错 情况      之前的figure图片消失了   
+//  class  变为 hide 了     query语句把所有的图片都隐藏了！！！
+    for(var i=0; i<allImages.length; i++){
+        allImages[i].className = "hide";
+    }
+
+    var picId = this.attributes["data-img"].value;
+    var pic = document.getElementById(picId);
+    if(pic.className === "hide"){
+        pic.className = "";
+    }else{
+        pic.className = "hide";
+    }
+}
+
+
+
+  
+
---
+++ js/add.js
@@ -0,0 +1,15 @@
+// do not reapeat yourself        dry
+
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var sum = document.getElementById("sum");
+
+num1.addEventListener("input", add);
+num2.addEventListener("input", add);
+
+function add () {
+    var one = parseFloat(num1.value) || 0;
+    var two = parseFloat(num2.value) || 0;
+
+    sum.innerHTML = "your sum is:" + (one + two);
+}
