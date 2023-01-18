7aedc3a2e9d352a586194fb35fb5f21c9a94a101
ShiinaRay <ShiinaRay@qq.com>
2023-01-18 23:07:18 +0800

    update

--- js/ShowPictures.js
+++ js/ShowPictures.js
@@ -8,11 +8,7 @@ elysia.addEventListener("click",picLink);
 silver.addEventListener("click",picLink);
 
 function picLink(){
-    var allImages = document.querySelectorAll(".ShowPictures img");    
-    //var allImages = document.querySelectorAll("img");  
-    // 之前把所有的  img标签都选中了    
-// f12  网页端  检查   出错 情况      之前的figure图片消失了   
-//  class  变为 hide 了     query语句把所有的图片都隐藏了！！！
+    var allImages = document.querySelectorAll(".ShowPictures img");
     for(var i=0; i<allImages.length; i++){
         allImages[i].className = "hide";
     }
